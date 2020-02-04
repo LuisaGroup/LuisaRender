60f879d35eeef94697b381580d722a68eac13bb4
mike <mango@live.cn>
2020-02-04 11:41:23 +0800

    working on parser

--- src/core/parser.h
+++ src/core/parser.h
@@ -25,65 +25,126 @@ private:
     std::string_view _derived_type_name;
     std::vector<std::string_view> _value_list;
     std::map<std::string_view, std::unique_ptr<ParameterSet>> _parameters;
+    
+    [[nodiscard]] static bool _parse_bool(std::string_view sv) {
+        if (sv == "true") {
+            return true;
+        } else if (sv == "false") {
+            return false;
+        }
+        LUISA_ERROR("invalid bool value: ", sv);
+    }
+    
+    [[nodiscard]] static float _parse_float(std::string_view sv) {
+        size_t offset = 0;
+        auto value = std::stof(std::string{sv}, &offset);
+        LUISA_ERROR_IF(offset != sv.size(), "invalid float value: ", sv);
+        return value;
+    }
+    
+    [[nodiscard]] static int32_t _parse_integer(std::string_view sv) {
+        size_t offset = 0;
+        auto value = std::stoi(std::string{sv}, &offset);
+        LUISA_ERROR_IF(offset != sv.size(), "invalid integer value: ", sv);
+        return value;
+    }
+    
+    [[nodiscard]] static std::string _parse_string(std::string_view sv) {
+        LUISA_ERROR_IF(sv.size() < 2 || sv.front() != sv.back() || (sv.front() != '"' && sv.front() != '\''), "invalid string value: ", sv);
+        auto raw = sv.substr(1, sv.size() - 2);
+        std::string value;
+        for (auto i = 0ul; i < raw.size(); i++) {
+            if (raw[i] != '\'' || ++i < raw.size()) {  // TODO: Smarter handling of escape characters
+                value.push_back(raw[i]);
+            } else {
+                LUISA_ERROR("extra escape at the end of string: ", sv);
+            }
+        }
+        return value;
+    }
 
 public:
     template<typename BaseClass>
-    [[nodiscard]] std::unique_ptr<BaseClass> parse_node(std::string_view parameter_name) const;
+    [[nodiscard]] std::shared_ptr<BaseClass> parse_node(std::string_view parameter_name) const;
+    
+    template<typename BaseClass>
+    [[nodiscard]] std::shared_ptr<BaseClass> parse_node_or_default(std::string_view parameter_name, const std::shared_ptr<BaseClass> &default_node) noexcept {
+        try { return parse_node<BaseClass>(parameter_name); } catch (const std::runtime_error &e) {
+            LUISA_WARNING("error occurred while parsing node: ", e.what());
+            return default_node;
+        }
+    }
     
     template<typename BaseClass>
     [[nodiscard]] std::vector<std::shared_ptr<BaseClass>> parse_node_list() const;
     
+    [[nodiscard]] bool parse_bool() const {
+        LUISA_ERROR_IF(_value_list.empty(), "no bool values given, expected exactly one");
+        LUISA_WARNING_IF(_value_list.size() != 1ul, "too many bool values, using only the first one");
+        return _parse_bool(_value_list.front());
+    }
+    
+    [[nodiscard]] bool parse_bool_or_default(bool default_value) const {
+        try { return parse_bool(); } catch (const std::runtime_error &e) {
+            LUISA_WARNING("error occurred while parsing bool: ", e.what());
+            return default_value;
+        }
+    }
+    
     [[nodiscard]] std::vector<bool> parse_bool_list() const {
         std::vector<bool> bool_list;
-        for (auto sv : _value_list) {
-            if (sv == "true") {
-                bool_list.emplace_back(true);
-            } else if (sv == "false") {
-                bool_list.emplace_back(false);
-            } else {
-                LUISA_ERROR("invalid bool value: ", sv);
-            }
-        }
+        for (auto sv : _value_list) { bool_list.emplace_back(_parse_bool(sv)); }
         return bool_list;
+    }
+    
+    [[nodiscard]] float parse_float() const {
+        LUISA_ERROR_IF(_value_list.empty(), "no float values given, expected exactly one");
+        LUISA_WARNING_IF(_value_list.size() != 1, "too many float values, using only the first one");
+        return _parse_float(_value_list.front());
+    }
+    
+    [[nodiscard]] float2 parse_float2() const {
+        LUISA_ERROR_IF(_value_list.size() < 2, "no enough float values given, expected exactly two");
+        LUISA_WARNING_IF(_value_list.size() != 2, "too many float values, using only the first two");
+        auto x = _parse_float(_value_list[0]);
+        auto y = _parse_float(_value_list[1]);
+        return make_float2(x, y);
+    }
+    
+    [[nodiscard]] float3 parse_float3() const {
+        LUISA_ERROR_IF(_value_list.size() < 3, "no enough float values given, expected exactly three");
+        LUISA_WARNING_IF(_value_list.size() != 3, "too many float values, using only the first three");
+        auto x = _parse_float(_value_list[0]);
+        auto y = _parse_float(_value_list[1]);
+        auto z = _parse_float(_value_list[2]);
+        return make_float3(x, y, z);
+    }
+    
+    [[nodiscard]] float4 parse_float4() const {
+        LUISA_ERROR_IF(_value_list.size() < 4, "no enough float values given, expected exactly four");
+        LUISA_WARNING_IF(_value_list.size() != 4, "too many float values, using only the first four");
+        auto x = _parse_float(_value_list[0]);
+        auto y = _parse_float(_value_list[1]);
+        auto z = _parse_float(_value_list[2]);
+        auto w = _parse_float(_value_list[3]);
+        return make_float4(x, y, z, w);
     }
     
     [[nodiscard]] std::vector<float> parse_float_list() const {
         std::vector<float> float_list;
-        for (auto sv : _value_list) {
-            size_t offset = 0;
-            auto value = std::stof(std::string{sv}, &offset);
-            LUISA_ERROR_IF(offset != sv.size(), "invalid float value: ", sv);
-            float_list.emplace_back(value);
-        }
+        for (auto sv : _value_list) { float_list.emplace_back(_parse_float(sv)); }
         return float_list;
     }
     
     [[nodiscard]] std::vector<int32_t> parse_integer_list() const {
         std::vector<int32_t> integer_list;
-        for (auto sv : _value_list) {
-            size_t offset = 0;
-            auto value = std::stoi(std::string{sv}, &offset);
-            LUISA_ERROR_IF(offset != sv.size(), "invalid integer value: ", sv);
-            integer_list.emplace_back(value);
-        }
+        for (auto sv : _value_list) { integer_list.emplace_back(_parse_integer(sv)); }
         return integer_list;
     }
     
     [[nodiscard]] std::vector<std::string> parse_string_list() const {
         std::vector<std::string> string_list;
-        for (auto sv : _value_list) {
-            LUISA_ERROR_IF(sv.size() < 2 || sv.front() != sv.back() || (sv.front() != '"' && sv.front() != '\''), "invalid string value: ", sv);
-            auto raw = sv.substr(1, sv.size() - 2);
-            std::string escaped;
-            for (auto i = 0ul; i < raw.size(); i++) {
-                if (raw[i] != '\'' || ++i < raw.size()) {  // TODO: Smarter handling of escape characters
-                    escaped.push_back(raw[i]);
-                } else {
-                    LUISA_ERROR("extra escape at the end of string: ", sv);
-                }
-            }
-            string_list.emplace_back(escaped);
-        }
+        for (auto sv : _value_list) { string_list.emplace_back(_parse_string(sv)); }
         return string_list;
     }
     
@@ -108,7 +169,7 @@ public:
 };
 
 template<typename BaseClass>
-std::unique_ptr<BaseClass> ParameterSet::parse_node(std::string_view parameter_name) const {
+std::shared_ptr<BaseClass> ParameterSet::parse_node(std::string_view parameter_name) const {
     if (auto iter = _parameters.find(parameter_name); iter != _parameters.end()) {
         return BaseClass::creators[iter->second->_derived_type_name](&_parser->device(), *iter->second);
     }
--- src/util/exception.h
+++ src/util/exception.h
@@ -12,7 +12,7 @@
     [&] { std::cerr << __FILE__ << ":" << __LINE__ << ": [WARNING] " << serialize(__VA_ARGS__) << std::endl; }()
     
 #define LUISA_ERROR(...)  \
-    [&] { throw std::runtime_error{serialize(__FILE__, ":", __LINE__, ": [ERROR] ", __VA_ARGS__)}; }()
+    throw std::runtime_error{serialize(__FILE__, ":", __LINE__, ": ", __VA_ARGS__)}
 
 #define LUISA_WARNING_IF(condition, ...)      [&] { if (condition) { LUISA_WARNING(__VA_ARGS__); } }()
 #define LUISA_WARNING_IF_NOT(condition, ...)  LUISA_WARNING_IF(!(condition), __VA_ARGS__)
