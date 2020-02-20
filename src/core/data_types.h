b7b80b6bd6c0f52488de4a345045756f4131c208
mike <mango@live.cn>
2020-02-20 12:16:37 +0800

    fixed make_float4x4(m00, ..., m33) on metal

--- src/core/data_types.h
+++ src/core/data_types.h
@@ -276,10 +276,10 @@ LUISA_DEVICE_CALLABLE inline auto make_float4x4(
     float m30, float m31, float m32, float m33) noexcept {
     
     return float4x4{
-        m00, m01, m02, m03,
-        m10, m11, m12, m13,
-        m20, m21, m22, m23,
-        m30, m31, m32, m33};
+        make_float4(m00, m01, m02, m03),
+        make_float4(m10, m11, m12, m13),
+        make_float4(m20, m21, m22, m23),
+        make_float4(m30, m31, m32, m33)};
 }
 
 LUISA_DEVICE_CALLABLE inline auto make_float4x4(float3x3 m) noexcept {
