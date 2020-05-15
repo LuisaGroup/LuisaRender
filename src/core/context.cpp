3792c19c696d6b133b225e3d29316e01803fe094
mike <mango@live.cn>
2020-05-15 09:34:09 +0800

    removed github workflow

---
+++ src/core/context.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mike Smith on 2020/5/15.
+//
+
+#include "context.h"
---
+++ src/core/context.h
@@ -0,0 +1,20 @@
+//
+// Created by Mike Smith on 2020/5/15.
+//
+
+#pragma once
+
+#include <filesystem>
+
+class Context {
+
+private:
+    std::filesystem::path _working_directory;
+    std::filesystem::path _binary_directory;
+
+public:
+
+
+};
+
+
