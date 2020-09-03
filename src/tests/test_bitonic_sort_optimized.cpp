d57bc203a03966a17c3a2b8a28e206e3a7e69caf
mike <mango@live.cn>
2020-09-03 22:25:19 +0800

    optimized bitonic sorting

--- src/tests/test_bitonic_sort_optimized.cpp
+++ src/tests/test_bitonic_sort_optimized.cpp
@@ -59,9 +59,9 @@ int main(int argc, char *argv[]) {
     constexpr const auto block_size = 1024u;
     
     auto small_stride_kernel = device->compile_kernel([&] {
-    
+        
         Arg<float *> data{buffer};
-    
+        
         Threadgroup<std::array<float, block_size>> cache;
         constexpr auto half_block_size = block_size / 2u;
         Auto tgid = thread_id() % half_block_size;
@@ -77,17 +77,17 @@ int main(int argc, char *argv[]) {
                 
                 Auto lhs_index = tid / half_cmp_step * cmp_step + tid % half_cmp_step;
                 Auto rhs_index = lhs_index + half_cmp_step;
-    
+                
                 Auto lhs = cache[lhs_index % block_size];
                 Auto rhs = cache[rhs_index % block_size];
                 
                 Auto reverse_ordered = lhs_index / cmp_stride % 2u;
-    
+                
                 Auto smaller = min(lhs, rhs);
                 Auto greater = max(lhs, rhs);
                 Auto ascending_pair = make_uint2(smaller, greater);
                 Auto descending_pair = make_uint2(greater, smaller);
-    
+                
                 Auto result = select(reverse_ordered == 1u, descending_pair, ascending_pair);
                 
                 cache[lhs_index % block_size] = result.x();
@@ -95,6 +95,50 @@ int main(int argc, char *argv[]) {
                 
                 threadgroup_barrier();
             }
+        }
+        data[thread_id() * 2u] = cache[tgid * 2u];
+        data[thread_id() * 2u + 1u] = cache[tgid * 2u + 1u];
+    });
+    
+    auto small_step_kernel = device->compile_kernel([&] {
+        
+        Arg<float *> data{buffer};
+        Arg<uint> cmp_step_in{&step};
+        Arg<uint> cmp_stride_in{&stride};
+        
+        Threadgroup<std::array<float, block_size>> cache;
+        constexpr auto half_block_size = block_size / 2u;
+        Auto tgid = thread_id() % half_block_size;
+        cache[tgid * 2u] = data[thread_id() * 2u];
+        cache[tgid * 2u + 1u] = data[thread_id() * 2u + 1u];
+        threadgroup_barrier();
+        
+        Auto tid = thread_id();
+        Auto cmp_stride = cmp_stride_in;
+        
+        for (auto cmp_step = block_size; cmp_step >= 2u; cmp_step /= 2) {
+            
+            auto half_cmp_step = cmp_step / 2u;
+            
+            Auto lhs_index = tid / half_cmp_step * cmp_step + tid % half_cmp_step;
+            Auto rhs_index = lhs_index + half_cmp_step;
+            
+            Auto lhs = cache[lhs_index % block_size];
+            Auto rhs = cache[rhs_index % block_size];
+            
+            Auto reverse_ordered = lhs_index / cmp_stride % 2u;
+            
+            Auto smaller = min(lhs, rhs);
+            Auto greater = max(lhs, rhs);
+            Auto ascending_pair = make_uint2(smaller, greater);
+            Auto descending_pair = make_uint2(greater, smaller);
+            
+            Auto result = select(reverse_ordered == 1u, descending_pair, ascending_pair);
+            
+            cache[lhs_index % block_size] = result.x();
+            cache[rhs_index % block_size] = result.y();
+            
+            threadgroup_barrier();
         }
         data[thread_id() * 2u] = cache[tgid * 2u];
         data[thread_id() * 2u + 1u] = cache[tgid * 2u + 1u];
@@ -108,9 +152,10 @@ int main(int argc, char *argv[]) {
         device->launch([&](Dispatcher &dispatch) {
             dispatch(small_stride_kernel->parallelize(buffer_size / 2u, block_size / 2u));
             for (stride = block_size * 2u; stride <= buffer_size; stride *= 2u) {
-                for (step = stride; step >= 2u; step /= 2) {
+                for (step = stride; step >= block_size * 2u; step /= 2) {
                     dispatch(kernel->parallelize(buffer_size / 2u, block_size / 2u));
                 }
+                dispatch(small_step_kernel->parallelize(buffer_size / 2u, block_size / 2u));
             }
         }, [i] { LUISA_INFO("Sorted #", i); });
     }
@@ -118,16 +163,8 @@ int main(int argc, char *argv[]) {
     device->synchronize();
     
     LUISA_INFO("Checking...");
-//    for (auto i = 0u; i + block_size <= buffer_size; i += block_size) {
-//        std::ostringstream ss;
-//        for (auto j = i; j < i + block_size; j++) {
-//            ss << host_buffer[j] << " ";
-//        }
-//        LUISA_INFO(ss.str());
-//    }
-    
-    for (auto i = 0u; i + block_size <= buffer_size; i += block_size) {
-        LUISA_ERROR_IF_NOT(std::is_sorted(host_buffer.cbegin() + i, host_buffer.cbegin() + i + block_size), "Fuck!");
+    for (auto i = 0u; i < buffer_size; i++) {
+        LUISA_ERROR_IF_NOT(host_buffer[i] == static_cast<float>(i), "Fuck!");
     }
     LUISA_INFO("Good!");
 }
