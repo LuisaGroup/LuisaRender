575d6c46d7fde189bfe861e6e8f8de02ff4f9887
Mike Smith <mango@live.cn>
2021-12-28 22:07:47 +0800

    added material and light flags

--- src/scene/shape.h
+++ src/scene/shape.h
@@ -16,7 +16,7 @@ struct alignas(16) VertexAttribute {
     uint compressed_tangent;
     float compressed_uv[2];
 
-    [[nodiscard]] static constexpr auto encode(float3 normal, float3 tangent, float2 uv) noexcept {
+    [[nodiscard]] static auto encode(float3 normal, float3 tangent, float2 uv) noexcept {
         constexpr auto oct_encode = [](float3 n) noexcept {
             constexpr auto oct_wrap = [](float2 v) noexcept {
                 return (1.0f - abs(v.yx())) * select(make_float2(-1.0f), make_float2(1.0f), v >= 0.0f);
