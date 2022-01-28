9e611923d8153d175b921681c1e8ec3987784eda
Mike Smith <mango@live.cn>
2022-01-28 23:27:25 +0800

    sikao

--- src/textures/hdr_illuminant.cpp
+++ src/textures/hdr_illuminant.cpp
@@ -17,29 +17,19 @@ class HDRIlluminantTexture final : public ImageTexture {
 
 private:
     std::shared_future<LoadedImage> _img;
-    std::array<float, 3u> _scale{};
-    bool _half{};
     bool _is_black{};
 
 private:
-    [[nodiscard]] float3 _v() const noexcept override {
-        return _half ?
-                   make_float3(_scale[0], _scale[1], _scale[2]) :
-                   make_float3(-1.0f);
-    }
+    [[nodiscard]] float3 _v() const noexcept override { return make_float3(); }
     [[nodiscard]] const LoadedImage &_image() const noexcept override { return _img.get(); }
     [[nodiscard]] Float4 _evaluate(
         const Pipeline &pipeline, const Var<TextureHandle> &handle,
         Expr<float2> uv, const SampledWavelengths &swl) const noexcept override {
-        auto L = pipeline.tex2d(handle->texture_id()).sample(uv);
+        auto rsp_scale = pipeline.tex2d(handle->texture_id()).sample(uv);
         auto scale = handle->v();
         auto spec = RGBIlluminantSpectrum{
-            RGBSigmoidPolynomial{L.xyz()}, L.w,
+            RGBSigmoidPolynomial{rsp_scale.xyz()}, rsp_scale.w,
             DenselySampledSpectrum::cie_illum_d6500()};
-        $if (scale.x >= 0.0f) {
-            spec = pipeline.srgb_illuminant_spectrum(
-                L.xyz() * scale);
-        };
         return spec.sample(swl);
     }
 
@@ -52,17 +42,28 @@ public:
                 return make_float3(desc->property_float_or_default(
                     "scale", 1.0f));
             }));
-        s = max(s, 0.0f);
-        _scale = {s.x, s.y, s.z};
-        _is_black = all(s == 0.0f);
-        _half = desc->property_bool_or_default("half", false);
-        _img = ThreadPool::global().async([path = std::move(path), half = _half, s] {
-            if (half) { return LoadedImage::load(path, PixelStorage::HALF4); }
+        _is_black = all(s <= 0.0f);
+        auto half = desc->property_bool_or_default("half", false);
+        _img = ThreadPool::global().async([path = std::move(path), half, s = max(s, 0.0f)] {
+            if (half) {
+                auto image = LoadedImage::load(path, PixelStorage::HALF4);
+                auto pixels = reinterpret_cast<std::array<uint16_t, 4u> *>(image.pixels());
+                for (auto i = 0u; i < image.size().x * image.size().y; i++) {
+                    auto [x, y, z, _] = pixels[i];
+                    auto f = make_float3(half_to_float(x), half_to_float(y), half_to_float(z));
+                    auto [rsp, scale] = RGB2SpectrumTable::srgb().decode_unbound(f * s);
+                    pixels[i] = {static_cast<uint16_t>(float_to_half(rsp.x)),
+                                 static_cast<uint16_t>(float_to_half(rsp.y)),
+                                 static_cast<uint16_t>(float_to_half(rsp.z)),
+                                 static_cast<uint16_t>(float_to_half(scale))};
+                }
+                return image;
+            }
             auto image = LoadedImage::load(path, PixelStorage::FLOAT4);
             auto pixels = reinterpret_cast<float4 *>(image.pixels());
             for (auto i = 0u; i < image.size().x * image.size().y; i++) {
-                auto p = pixels[i];
-                auto [rsp, scale] = RGB2SpectrumTable::srgb().decode_unbound(p.xyz() * s);
+                auto p = pixels[i].xyz() * s;
+                auto [rsp, scale] = RGB2SpectrumTable::srgb().decode_unbound(p);
                 pixels[i] = make_float4(rsp, scale);
             }
             return image;
