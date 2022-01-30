7bc46f1d79967a305ef9980ef25a9ea1f8387274
Mike Smith <mango@live.cn>
2022-01-30 11:31:56 +0800

    renaming...

--- src/textures/constant_illuminant.cpp
+++ src/textures/constant_illuminant.cpp
@@ -10,13 +10,12 @@ namespace luisa::render {
 class ConstantIlluminant final : public Texture {
 
 private:
-    std::array<float, 3> _rsp{};
-    float _scale{};
+    float4 _rsp_scale;
 
 private:
     [[nodiscard]] TextureHandle _encode(Pipeline &pipeline, CommandBuffer &command_buffer, uint handle_tag) const noexcept override {
         auto [buffer, buffer_id] = pipeline.arena_buffer<float4>(1u);
-        command_buffer << buffer.copy_from(&_rsp);
+        command_buffer << buffer.copy_from(&_rsp_scale);
         return TextureHandle::encode_texture(handle_tag, buffer_id);
     }
 
@@ -35,10 +34,7 @@ public:
             }));
         auto rsp_scale = RGB2SpectrumTable::srgb().decode_unbound(
             max(color, 0.0f) * max(scale, 0.0f));
-        _rsp[0] = rsp_scale.first.x;
-        _rsp[1] = rsp_scale.first.y;
-        _rsp[2] = rsp_scale.first.z;
-        _scale = rsp_scale.second;
+        _rsp_scale = make_float4(rsp_scale.first, rsp_scale.second);
     }
     [[nodiscard]] luisa::string_view impl_type() const noexcept override { return "constillum"; }
     [[nodiscard]] Float4 evaluate(
@@ -46,7 +42,7 @@ public:
         const Var<TextureHandle> &handle, Expr<float>) const noexcept override {
         return pipeline.buffer<float4>(handle->texture_id()).read(0u);
     }
-    [[nodiscard]] bool is_black() const noexcept override { return _scale == 0.0f; }
+    [[nodiscard]] bool is_black() const noexcept override { return _rsp_scale.w == 0.0f; }
     [[nodiscard]] Category category() const noexcept override { return Category::ILLUMINANT; }
 };
 
