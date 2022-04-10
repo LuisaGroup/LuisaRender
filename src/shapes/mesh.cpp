fdae62edc8fc38f253c619591245f6ce4e3d3644
Mike Smith <mango@live.cn>
2022-04-10 16:34:20 +0800

    minor

--- src/shapes/mesh.cpp
+++ src/shapes/mesh.cpp
@@ -6,6 +6,7 @@
 #include <assimp/postprocess.h>
 #include <assimp/scene.h>
 #include <assimp/mesh.h>
+#include <assimp/Subdivision.h>
 
 #include <core/thread_pool.h>
 #include <base/shape.h>
@@ -24,32 +25,28 @@ public:
     [[nodiscard]] auto triangles() const noexcept { return luisa::span{_triangles}; }
     [[nodiscard]] auto has_uv() const noexcept { return _has_uv; }
 
-    [[nodiscard]] static auto load(std::filesystem::path path) noexcept {
+    [[nodiscard]] static auto load(std::filesystem::path path, uint subdiv_level) noexcept {
 
-        return ThreadPool::global().async([path = std::move(path)] {
+        return ThreadPool::global().async([path = std::move(path), subdiv_level] {
             Clock clock;
             auto path_string = path.string();
             Assimp::Importer importer;
             importer.SetPropertyInteger(
-                AI_CONFIG_PP_RVC_FLAGS,
-                aiComponent_ANIMATIONS | aiComponent_BONEWEIGHTS |
-                    aiComponent_CAMERAS | aiComponent_COLORS |
-                    aiComponent_LIGHTS | aiComponent_MATERIALS |
-                    aiComponent_TEXTURES);
-            importer.SetPropertyInteger(
-                AI_CONFIG_PP_SBP_REMOVE,
-                aiPrimitiveType_LINE | aiPrimitiveType_POINT);
-            importer.SetPropertyBool(
-                AI_CONFIG_PP_FD_CHECKAREA, false);
-            auto model = importer.ReadFile(
-                path_string.c_str(),
-                aiProcess_Triangulate | aiProcess_JoinIdenticalVertices |
-                    aiProcess_RemoveComponent | aiProcess_ImproveCacheLocality |
-                    aiProcess_OptimizeGraph | aiProcess_GenNormals |
-                    aiProcess_GenUVCoords | aiProcess_FlipUVs |
-                    aiProcess_TransformUVCoords | aiProcess_FixInfacingNormals |
-                    aiProcess_RemoveRedundantMaterials | aiProcess_FindInvalidData |
-                    aiProcess_SortByPType | aiProcess_FindDegenerates);
+                AI_CONFIG_PP_SBP_REMOVE, aiPrimitiveType_LINE | aiPrimitiveType_POINT);
+            importer.SetPropertyBool(AI_CONFIG_PP_FD_CHECKAREA, false);
+            auto import_flags = aiProcess_JoinIdenticalVertices | aiProcess_RemoveComponent |
+                                aiProcess_OptimizeGraph | aiProcess_GenUVCoords |
+                                aiProcess_FlipUVs | aiProcess_TransformUVCoords |
+                                aiProcess_FixInfacingNormals | aiProcess_RemoveRedundantMaterials |
+                                aiProcess_FindInvalidData | aiProcess_SortByPType | aiProcess_GenNormals |
+                                aiProcess_FindDegenerates | aiProcess_ImproveCacheLocality;
+            auto remove_flags = aiComponent_ANIMATIONS | aiComponent_BONEWEIGHTS |
+                                aiComponent_CAMERAS | aiComponent_COLORS |
+                                aiComponent_LIGHTS | aiComponent_MATERIALS |
+                                aiComponent_TEXTURES;
+            if (subdiv_level == 0) { import_flags |= aiProcess_Triangulate; }
+            importer.SetPropertyInteger(AI_CONFIG_PP_RVC_FLAGS, static_cast<int>(remove_flags));
+            auto model = importer.ReadFile(path_string.c_str(), import_flags);
             if (model == nullptr || (model->mFlags & AI_SCENE_FLAGS_INCOMPLETE) ||
                 model->mRootNode == nullptr || model->mRootNode->mNumMeshes == 0) [[unlikely]] {
                 LUISA_ERROR_WITH_LOCATION(
@@ -57,16 +54,13 @@ public:
                     path_string, importer.GetErrorString());
             }
             auto mesh = model->mMeshes[0];
-            if (auto uv_count = std::count_if(
-                    std::cbegin(mesh->mTextureCoords),
-                    std::cend(mesh->mTextureCoords),
-                    [](auto p) noexcept { return p != nullptr; });
-                uv_count > 1) [[unlikely]] {
-                LUISA_WARNING_WITH_LOCATION(
-                    "More than one set of texture coordinates "
-                    "found in mesh '{}'. Only the first set "
-                    "will be considered.",
-                    path_string);
+            if (subdiv_level > 0u) {
+                auto subdiv = Assimp::Subdivider::Create(Assimp::Subdivider::CATMULL_CLARKE);
+                aiMesh *subdiv_mesh = nullptr;
+                subdiv->Subdivide(mesh, subdiv_mesh, subdiv_level, true);
+                model->mMeshes[0] = nullptr;
+                mesh = subdiv_mesh;
+                delete subdiv;
             }
             if (mesh->mTextureCoords[0] == nullptr ||
                 mesh->mNumUVComponents[0] != 2) [[unlikely]] {
@@ -74,14 +68,18 @@ public:
                     "Invalid texture coordinates in mesh '{}': "
                     "address = {}, components = {}.",
                     path_string,
-                    fmt::ptr(mesh->mTextureCoords[0]),
+                    static_cast<void *>(mesh->mTextureCoords[0]),
                     mesh->mNumUVComponents[0]);
             }
             MeshLoader loader;
+            luisa::vector<aiVector3t<ai_real>> computed_normals;
             auto vertex_count = mesh->mNumVertices;
             auto ai_positions = mesh->mVertices;
             auto ai_normals = mesh->mNormals;
             auto ai_tex_coords = mesh->mTextureCoords[0];
+            constexpr auto to_float3 = [](aiVector3t<ai_real> v) noexcept {
+                return make_float3(v.x, v.y, v.z);
+            };
             loader._vertices.resize(vertex_count);
             loader._has_uv = ai_tex_coords != nullptr;
             auto compute_uv = [ai_tex_coords](auto i) noexcept {
@@ -89,8 +87,8 @@ public:
                 return make_float2(ai_tex_coords[i].x, ai_tex_coords[i].y);
             };
             for (auto i = 0; i < vertex_count; i++) {
-                auto p = make_float3(ai_positions[i].x, ai_positions[i].y, ai_positions[i].z);
-                auto n = make_float3(ai_normals[i].x, ai_normals[i].y, ai_normals[i].z);
+                auto p = to_float3(ai_positions[i]);
+                auto n = to_float3(ai_normals[i]);
                 loader._vertices[i].pos = p;
                 loader._vertices[i].compressed_normal =
                     Shape::Vertex::oct_encode(normalize(n));
@@ -98,14 +96,23 @@ public:
                 loader._vertices[i].compressed_uv[0] = uv.x;
                 loader._vertices[i].compressed_uv[1] = uv.y;
             }
-            auto triangle_count = mesh->mNumFaces;
-            auto ai_triangles = mesh->mFaces;
-            loader._triangles.resize(triangle_count);
-            std::transform(
-                ai_triangles, ai_triangles + triangle_count, loader._triangles.begin(),
-                [](const aiFace &face) noexcept {
-                    return Triangle{face.mIndices[0], face.mIndices[1], face.mIndices[2]};
-                });
+            if (subdiv_level == 0u) {
+                auto ai_triangles = mesh->mFaces;
+                loader._triangles.resize(mesh->mNumFaces);
+                std::transform(
+                    ai_triangles, ai_triangles + mesh->mNumFaces, loader._triangles.begin(),
+                    [](const aiFace &face) noexcept {
+                        return Triangle{face.mIndices[0], face.mIndices[1], face.mIndices[2]};
+                    });
+            } else {
+                auto ai_quads = mesh->mFaces;
+                loader._triangles.resize(mesh->mNumFaces * 2u);
+                for (auto i = 0u; i < mesh->mNumFaces; i++) {
+                    auto &face = ai_quads[i];
+                    loader._triangles[i * 2u + 0u] = {face.mIndices[0], face.mIndices[1], face.mIndices[2]};
+                    loader._triangles[i * 2u + 1u] = {face.mIndices[0], face.mIndices[2], face.mIndices[3]};
+                }
+            }
 
             // compute tangent space (code adapted from PBRT-v4).
             if (loader._has_uv) {
@@ -151,8 +158,7 @@ public:
                 }
             } else {
                 for (auto i = 0u; i < vertex_count; i++) {
-                    auto n = make_float3(
-                        ai_normals[i].x, ai_normals[i].y, ai_normals[i].z);
+                    auto n = to_float3(ai_normals[i]);
                     auto b = abs(n.x) > abs(n.z) ?
                                  make_float3(-n.y, n.x, 0.0f) :
                                  make_float3(0.0f, -n.z, n.y);
@@ -175,7 +181,10 @@ private:
 
 public:
     Mesh(Scene *scene, const SceneNodeDesc *desc) noexcept
-        : Shape{scene, desc}, _loader{MeshLoader::load(desc->property_path("file"))} {}
+        : Shape{scene, desc},
+          _loader{MeshLoader::load(
+              desc->property_path("file"),
+              desc->property_uint_or_default("subdivision", 0u))} {}
     [[nodiscard]] luisa::string_view impl_type() const noexcept override { return LUISA_RENDER_PLUGIN_NAME; }
     [[nodiscard]] luisa::span<const Shape *const> children() const noexcept override { return {}; }
     [[nodiscard]] bool deformable() const noexcept override { return false; }
