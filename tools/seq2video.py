96926903dc6694b436e2c4fa6c730f57fededb6e
chenxin <chenxin18@mails.tsinghua.edu.cn>
2022-04-12 14:52:26 +0800

    fix seq2video

--- tools/seq2video.py
+++ tools/seq2video.py
@@ -11,25 +11,42 @@ def rgb2srgb(image):
     ) * 255, 0, 255)))
 
 
+def exr2png(images):
+    for i, image in enumerate(images):
+        print(f"Transforming frame {i}")
+        frame = rgb2srgb(image)
+        cv.imwrite(f"{folder}/{files[i][:-4]}.png", frame)
+
+
+def png2video(images, frame_rate):
+    writer = cv.VideoWriter(f"{folder}/output.mp4", cv.VideoWriter_fourcc('m', 'p', '4', 'v'),
+                            frame_rate, (images[0].shape[1], images[0].shape[0]))
+    # writer = cv.VideoWriter(f"{folder}/output.avi", cv.VideoWriter_fourcc('M', 'J', 'P', 'G'),
+    #                         frame_rate, (images[0].shape[1], images[0].shape[0]))
+    for i, image in enumerate(images):
+        print(f"Processing frame {i}")
+        writer.write(image)
+
+    writer.release()
+    cv.waitKey()
+    cv.destroyAllWindows()
+
+
 if __name__ == "__main__":
     environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
     import cv2 as cv
 
     folder = argv[1]
-    frame_rate = 10
+    frame_rate = argv[2]
 
     files = sorted(f for f in listdir(folder) if f.endswith(".exr") and not f.startswith("dump-"))
     print(f"Reading images from '{folder}'...")
     images = [cv.imread(f"{folder}/{f}", cv.IMREAD_UNCHANGED) for f in files]
-    writer = cv.VideoWriter(f"{folder}/output.mp4", cv.VideoWriter_fourcc('m', 'p', '4', 'v'),
-                            frame_rate, (images[0].shape[1], images[0].shape[0]))
-    for i, image in enumerate(images):
-        print(f"Processing frame {i}")
-        frame = rgb2srgb(image)
-        cv.imshow("Display", frame)
-        cv.waitKey(1)
-        cv.imwrite(f"{folder}/{files[i][:-4]}.png", frame)
-        writer.write(frame)
-    writer.release()
-    cv.waitKey()
-    cv.destroyAllWindows()
+    exr2png(images)
+
+    print("")
+
+    files = sorted(f for f in listdir(folder) if f.endswith(".png") and not f.startswith("dump-"))
+    print(f"Reading images from '{folder}'...")
+    images = [cv.imread(f"{folder}/{f}", cv.IMREAD_COLOR) for f in files]
+    png2video(images, frame_rate)
