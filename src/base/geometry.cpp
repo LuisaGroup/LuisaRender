45b0373a5f7a7416d6a2871e85a13819561dcadd
Mike Smith <mango@live.cn>
2022-09-14 13:05:40 +0800

    WIP: refactor Pipeline

---
+++ src/base/geometry.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Mike Smith on 2022/9/14.
+//
+
+#include <base/geometry.h>
+
+namespace luisa::render {
+
+}
---
+++ src/base/geometry.h
@@ -0,0 +1,12 @@
+//
+// Created by Mike Smith on 2022/9/14.
+//
+
+#pragma once
+
+namespace luisa::render {
+
+class Geometry {
+};
+
+}// namespace luisa::render
