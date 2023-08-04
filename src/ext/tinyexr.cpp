da38422200c3be7121bfaaf5ade69b77bc66bdde
Mike Smith <mango@live.cn>
2023-08-04 15:38:39 +0800

    use system zlib for tinyexr if found

---
+++ src/ext/tinyexr.cpp
@@ -0,0 +1,12 @@
+#if defined(_WIN32)
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#endif
+
+#if !defined(TINYEXR_USE_MINIZ) || TINYEXR_USE_MINIZ == 0
+#include <zlib.h>
+#endif
+
+#define TINYEXR_IMPLEMENTATION
+#include "tinyexr.h"
