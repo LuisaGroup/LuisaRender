141e9b9acc8d68982b96efe1e34af3723a3c8cc3
kanglf20 <kanglf20@mails.tsinghua.edu.cn>
2024-04-13 01:55:15 +0800

    RIS over DI samples

--- src/integrators/restir_di.cpp
+++ src/integrators/restir_di.cpp
@@ -19,26 +19,21 @@ using namespace compute;
 class ReSTIRDirectLighting final : public ProgressiveIntegrator {
 
 private:
-    uint _num_nee_sample;
-    uint _num_bsdf_sample;
+    uint _num_initial_sample;
     uint _num_neighbor_sample;
-    /*
-     * temporal reuse currently disabled for (offline) progressive rendering
-     * bool _enable_temporal_reuse;
-     */
+    bool _enable_temporal_reuse;
     bool _enable_spatial_reuse;
     bool _enable_visibility_reuse;
 
 public:
     ReSTIRDirectLighting(Scene *scene, const SceneNodeDesc *desc) noexcept
         : ProgressiveIntegrator{scene, desc},
-          _num_nee_sample{desc->property_uint_or_default("num_nee_sample", 1u)},
-          _num_bsdf_sample{desc->property_uint_or_default("num_bsdf_sample", 1u)},
+          _num_initial_sample{desc->property_uint_or_default("num_initial_sample", 1u)},
           _num_neighbor_sample{desc->property_uint_or_default("num_neighbor_sample", 5u)},
+          _enable_temporal_reuse{desc->property_bool_or_default("enable_temporal_reuse", true)},
           _enable_spatial_reuse{desc->property_bool_or_default("enable_spatial_reuse", true)},
           _enable_visibility_reuse{desc->property_bool_or_default("enable_visibility_reuse", true)} {}
-    [[nodiscard]] auto num_nee_sample() const noexcept { return _num_nee_sample; }
-    [[nodiscard]] auto num_bsdf_sample() const noexcept { return _num_bsdf_sample; }
+    [[nodiscard]] auto num_initial_sample() const noexcept { return _num_initial_sample; }
     [[nodiscard]] auto num_neighbor_sample() const noexcept { return _num_neighbor_sample; }
     [[nodiscard]] auto enable_spatial_reuse() const noexcept { return _enable_spatial_reuse; }
     [[nodiscard]] auto enable_visibility_reuse() const noexcept { return _enable_visibility_reuse; }
@@ -52,6 +47,109 @@ class ReSTIRDirectLightingInstance final : public ProgressiveIntegrator::Instanc
 public:
     using ProgressiveIntegrator::Instance::Instance;
 
+    struct ReservoirSample {
+        UInt tag;
+        Float2 u_light_selection;
+    };
+    struct ReservoirWeight {
+        Float m;
+        Float total_weight;
+        Float target_pdf;
+    };
+    struct Reservoir {
+        ReservoirSample sample;
+        ReservoirWeight weight;
+        [[nodiscard]] static auto zero() noexcept {
+            return Reservoir{ReservoirSample{0u, make_float2(0.f)}, ReservoirWeight{0.f, 0.f, 0.f}};
+        }
+        [[nodiscard]] auto contribution_weight() const noexcept {
+            return ite(weight.total_weight == 0.f, 0.f, weight.total_weight / weight.target_pdf);
+        }
+        void update(const Reservoir &r, Expr<float> u_sel) noexcept {
+            auto w1 = weight.total_weight * weight.m,
+                 w2 = r.weight.total_weight * r.weight.m;
+            weight.m = weight.m + r.weight.m;
+            weight.total_weight = w1 + w2;
+            $if (u_sel * weight.total_weight < w2) {
+                sample = r.sample;
+                weight.target_pdf = r.weight.target_pdf;
+            };
+            weight.total_weight /= weight.m;
+        }
+        void update(const Reservoir &r, Expr<float> m1, Expr<float> m2, Expr<float> u_sel) noexcept {
+            auto w1 = weight.total_weight * m1, w2 = r.weight.total_weight * m2;
+            weight.m = weight.m + r.weight.m;
+            weight.total_weight = w1 + w2;
+            $if (u_sel * weight.total_weight < w2) {
+                sample = r.sample;
+                weight.target_pdf = r.weight.target_pdf;
+            };
+        }
+    };
+private:
+    [[nodiscard]] std::pair<SampledSpectrum, Float> _evaluate_reservoir_sample(const ReservoirSample &sample, const Interaction &it, Expr<float3> wo,
+                                                                               const SampledWavelengths &swl, Expr<float> time, Expr<bool> occlusion_test = false) const noexcept {
+        auto L = SampledSpectrum{swl.dimension(), 0.f};
+        auto pdf = def(0.f);
+        auto prob = light_sampler()->evaluate_selection(sample.tag, it.p(), swl, time);
+        auto sel = LightSampler::Selection{sample.tag, prob};
+        auto light_sample = light_sampler()->sample_light(it, sel, sample.u_light_selection, swl, time);
+        auto occluded = def(false);
+        $if(occlusion_test) {
+            occluded = pipeline().geometry()->intersect_any(light_sample.shadow_ray);
+        };
+        $if(light_sample.eval.pdf > 0.f & !occluded) {
+            auto surface_tag = it.shape().surface_tag();
+            PolymorphicCall<Surface::Closure> call;
+            pipeline().surfaces().dispatch(surface_tag, [&](auto surface) noexcept {
+                surface->closure(call, it, swl, wo, 1.f, time);
+            });
+            call.execute([&](auto closure) noexcept {
+                auto wi = light_sample.shadow_ray->direction();
+                auto eval = closure->evaluate(wo, wi);
+                pdf = light_sample.eval.pdf;
+                L = eval.f * light_sample.eval.L;
+            });
+        };
+        return std::make_pair(L, pdf);
+    }
+    [[nodiscard]] Reservoir _generate_reservoir(const Interaction &it, Expr<float3> wo, const SampledWavelengths &swl, Expr<float> time) const noexcept {
+        auto u_sel = sampler()->generate_1d();
+        auto sel = light_sampler()->select(it, u_sel, swl, time);
+        auto u_light_selection = sampler()->generate_2d();
+        auto reservoir_sample = ReservoirSample{sel.tag, u_light_selection};
+        auto [L, pdf] = _evaluate_reservoir_sample(reservoir_sample, it, wo, swl, time);
+        auto target_pdf = L.sum();
+        auto total_weight = ite(pdf == 0.f, 0.f, target_pdf / pdf);
+        return Reservoir{reservoir_sample, ReservoirWeight{1.f, total_weight, target_pdf}};
+    }
+public:
+    class ReservoirBuffer {
+    private:
+        uint2 _resolution;
+        Buffer<uint3> _sample;
+        Buffer<float3> _weight;
+    public:
+        ReservoirBuffer(const Pipeline &pipeline, uint2 resolution) noexcept
+            : _resolution{resolution} {
+            auto num_pixel = resolution.x * resolution.y;
+            _sample = pipeline.device().create_buffer<uint3>(num_pixel);
+            _weight = pipeline.device().create_buffer<float3>(num_pixel);
+        }
+        [[nodiscard]] auto read(Expr<uint2> pixel_id) const noexcept {
+            auto sample = _sample->read(pixel_id.x + pixel_id.y * _resolution.x);
+            auto weight = _weight->read(pixel_id.x + pixel_id.y * _resolution.x);
+            auto reservoir_sample = ReservoirSample{sample.x, weight.xy().as<float2>()};
+            auto reservoir_weight = ReservoirWeight{weight.z, weight.y, weight.x};
+            return Reservoir{reservoir_sample, reservoir_weight};
+        }
+        void write(const Reservoir &r, Expr<uint2> pixel_id) noexcept {
+            auto sample = make_uint3(r.sample.tag, r.sample.u_light_selection.as<uint2>());
+            auto weight = make_float3(r.weight.m, r.weight.total_weight, r.weight.target_pdf);
+            _sample->write(pixel_id.x + pixel_id.y * _resolution.x, sample);
+            _weight->write(pixel_id.x + pixel_id.y * _resolution.x, weight);
+        }
+    };
 protected:
     void _render_one_camera(CommandBuffer &command_buffer,
                             Camera::Instance *camera) noexcept override {
@@ -73,7 +171,7 @@ protected:
             "Rendering to '{}' of resolution {}x{} at {}spp.",
             image_file.string(),
             resolution.x, resolution.y, spp);
-
+        auto spatial_reservoir_buffer = luisa::make_unique<ReservoirBuffer>(pipeline(), resolution);
         using namespace luisa::compute;
         // Kernel2D temporal_reuse_kernel
         // Kernel2D spatial_reuse_kernel
@@ -178,33 +276,14 @@ protected:
 
             // compute direct lighting
             $if(!it->shape().has_surface()) { $break; };
-            auto u_light_selection = sampler()->generate_1d();
-            auto u_light_surface = sampler()->generate_2d();
-            auto sel = light_sampler()->select(*it, u_light_selection, swl, time);
-            auto light_sample = light_sampler()->sample_light(*it, sel, u_light_surface, swl, time);
-            auto occluded = def(false);
-            $if(light_sample.eval.pdf > 0.f) {
-                occluded = pipeline().geometry()->intersect_any(light_sample.shadow_ray);
+            auto reservoir = Reservoir::zero();
+            $for(_, node<ReSTIRDirectLighting>()->num_initial_sample()) {
+                auto candidate = _generate_reservoir(*it, wo, swl, time);
+                reservoir.update(candidate, sampler()->generate_1d());
             };
-
-            // evaluate material
-            auto surface_tag = it->shape().surface_tag();
-            PolymorphicCall<Surface::Closure> call;
-            pipeline().surfaces().dispatch(surface_tag, [&](auto surface) noexcept {
-                surface->closure(call, *it, swl, wo, 1.f, time);
-            });
-            call.execute([&](auto closure) noexcept {
-                if (auto dispersive = closure->is_dispersive()) {
-                    $if(*dispersive) { swl.terminate_secondary(); };
-                }
-                $if(light_sample.eval.pdf > 0.0f & !occluded) {
-                    auto wi = light_sample.shadow_ray->direction();
-                    auto eval = closure->evaluate(wo, wi);
-                    $if(eval.pdf > 0.f) {
-                        Li += cs.weight * eval.f * light_sample.eval.L / light_sample.eval.pdf;
-                    };
-                };
-            });
+            auto [L, _] = _evaluate_reservoir_sample(reservoir.sample, *it, wo, swl, time, true);
+            auto contribution_weight = reservoir.contribution_weight();
+            Li += cs.weight * contribution_weight * L;
             $break;
         };
         return spectrum->srgb(swl, Li);
